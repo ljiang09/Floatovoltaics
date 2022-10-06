7d762eddaccbe10734f5c40720a6ea51ef9044f8
Brooke Moss <me@brookem.dev>
2022-10-06 13:19:30 -0400

    Equation things

--- lake_mead_model.py
+++ lake_mead_model.py
@@ -1,13 +1,26 @@
 
+DENSITY = 1000 # Density of water (kg/m^3)
+A_max = 640000000 # m^2
+V_max = 32236000000 # m^3
+
+x_s = 0.014659 # maximum humidity ratio of saturated air at the same temperature as the water surface (kg/kg)  (kg H2O in kg Dry Air)
+#! ^^ placeholder value
+x = # humidity ratio air (kg/kg) (kg H2O in kg Dry Air)
+
 V_in = 12000	# ft^3 / s
 V_dam = 8000	# ft^3 / s
-# V_evap = cndjdnajkcl # this will be a function of surface area, which we can calculate from V/h
+V_evap = cndjdnajkcl # this will be a function of surface area, which we can calculate from V/h
 
-dV_dt = V_in - V_dam - V_evap
+v_air = 10 #! placeholder value
 
 
-# Rate of evaporation of water, g_s. Units of kg/s
+dVdt = V_in - V_dam - V_evap
+
+
+# kg / (m^2 * h)
 Theta = 25+19*v_air			# v_air = velocity of air
-# A = ncdjkncsjndk    # kg / (m^2 * h)
-g_s = Theta*A*(x_s - x) / 3600
+  
+# Rate of evaporation of water, g_s. Units of kg/s
+A = 
+V_evap = Theta*A*(x_s - x) / (3600 * DENSITY)
 
