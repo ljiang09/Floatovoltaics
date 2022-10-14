0d7d551db05fdd97b0ff66190d765cbf6a80a3b4
Brooke Moss <me@brookem.dev>
2022-10-14 09:43:21 -0400

    In class changes 10/13 Includes changing to seconds instead of hours

--- lake_mead_model.py
+++ lake_mead_model.py
@@ -15,31 +15,32 @@ def model(V, t, V_in, V_dam):
     DENSITY = 1000  # Density of water (kg/m^3)
 
     # maximum humidity ratio of saturated air at the same temperature as the water surface (kg/kg)  (kg H2O in kg Dry Air)
-    MAX_HUM_RATIO = 0.014659
-    HUM_RATIO = 0.01  # humidity ratio air (kg/kg) (kg H2O in kg Dry Air)
+    MAX_HUM_RATIO = 0.030
+    HUM_RATIO = 0.015  # humidity ratio air (kg/kg) (kg H2O in kg Dry Air)
     #! ^^ placeholder values
 
     LENGTH = 3.18  # meters
     area = (2*V) / math.sqrt(V/LENGTH)
 
-    v_air = 10  # ! placeholder value
+    v_air = 10  # m/s
 
     # kg / (m^2 * h)
     Theta = 25+(19*v_air)			# v_air = velocity of air
 
     # this will be a function of surface area, which we can calculate from V/h
-    V_evap = Theta*area*(MAX_HUM_RATIO - HUM_RATIO) / (DENSITY)
+    V_evap = Theta*area*(MAX_HUM_RATIO - HUM_RATIO) / (DENSITY * 3600)
+    
     # dVdt = V_in - V_dam - V_evap
-    dVdt = -V_evap
-    print(dVdt)
+    dVdt = V_evap
+    print(V_evap/V_in)
     return dVdt
 
 
-def cfsToM3Hr(cfs):
+def cfsToM3s(cfs):
     '''
     Insert docstrings here
     '''
-    return cfs * 0.028316846711688 * 3600
+    return cfs * 0.028316846711688
 
 
 def m3ToKm3(m3):
@@ -49,19 +50,19 @@ def m3ToKm3(m3):
     return m3 / (1000000000)
 
 
-V_in = cfsToM3Hr(11800)  # m^3 / hr
-V_dam = cfsToM3Hr(11600)  # m^3 / hr
+V_in = cfsToM3s(11800)  # m^3 / s
+V_dam = cfsToM3s(9800)  # m^3 / s
 
 V_0 = 10000000000  # ! Placeholder, m^3
 
-t = np.linspace(0, 20000, 20)
+t = np.linspace(0, 31536000)
 
 result = odeint(model, V_0, t, args=(V_in, V_dam))
 
 fig, ax = plt.subplots()
-ax.plot(t, m3ToKm3(result), label='V_0 = 10km^3')
+ax.plot(t/86400, m3ToKm3(result), label='V_0 = 10km^3')
 ax.legend()
-ax.set_xlabel('Time (hrs)')
+ax.set_xlabel('Time (days)')
 ax.set_ylabel('Volume (km^3)')
 
 plt.show()
