428ee5eb4d8547642d8e6748750361a47ec320e0
Brooke Moss <me@brookem.dev>
2022-10-15 14:22:05 -0400

    sweep

--- lake_mead_model.py
+++ lake_mead_model.py
@@ -7,6 +7,9 @@ from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 import numpy as np
 
+result = []
+panel_areas = [10000, 100000, 1000000] # m^2
+
 
 def model(V, t, V_in, V_dam):
     '''
@@ -28,10 +31,10 @@ def model(V, t, V_in, V_dam):
     Theta = 25+(19*v_air)			# v_air = velocity of air
 
     # this will be a function of surface area, which we can calculate from V/h
-    V_evap = Theta*area*(MAX_HUM_RATIO - HUM_RATIO) / (DENSITY * 3600)
+    V_evap = Theta*(area-panel_area)*(MAX_HUM_RATIO - HUM_RATIO) / (DENSITY * 3600)
     
-    # dVdt = V_in - V_dam - V_evap
-    dVdt = V_evap
+    dVdt = V_in - V_dam - V_evap
+    # dVdt = V_evap
     print(V_evap/V_in)
     return dVdt
 
@@ -60,10 +63,13 @@ V_0 = 10000000000  # ! Placeholder, m^3
 
 t = np.linspace(0, 31536000)
 
-result = odeint(model, V_0, t, args=(V_in, V_dam))
-
 fig, ax = plt.subplots()
-ax.plot(t/86400, m3ToKm3(result), label='V_0 = 10km^3')
+
+for i, panel_area in panel_areas:
+
+    result[i] = odeint(model, V_0, t, args=(V_in, V_dam))
+    ax.plot(t/86400, m3ToKm3(result), label=f'Panel Area = {panel_area}m^2')
+
 ax.legend()
 ax.set_xlabel('Time (days)')
 ax.set_ylabel('Volume (km^3)')
