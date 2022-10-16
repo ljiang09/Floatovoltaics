88c4f3f2656772c4530b4de20d3b17751da42f00
Lily Jiang <ljiang@olin.edu>
2022-10-15 21:13:56 -0400

    add cost model script

---
+++ cost_model.py
@@ -0,0 +1,17 @@
+'''
+Compares the cost of solar panels to the revenue from selling the
+energy, signifying if it is worth it to spend money to install panels.
+Also accounts for the value of lost water from evaporation.
+'''
+
+
+# sweeping the number of solar panels for one volume level
+
+number of solar panels for various volume levels
+
+sweep number of solar panels 
+
+
+Sweep through volume rn for how many solar panels vs ___
+
+Look into conversion factors for how much lost water is worth to us. How much solar generation 
